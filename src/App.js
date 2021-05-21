7942cb722db3316de1bb94f30caa36023c9083ed
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-05-20 19:43:43 -0500

    Tareas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "",
+    list:[]};
+  }
+
+ 
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  sendName(event) {
+    event.preventDefault();
+    this.setState({
+      list: this.state.list.concat(this.state.name)
+    });
+  }
+  
+
   render() {
     return (
       <div className="wrapper">
@@ -12,14 +33,18 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+              {this.state.list.map((value)=>
+                <li key={value}>{value}</li>
+              )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.sendName.bind(this)}>
+             <input type="text" id="new-task" value={this.state.name} placeholder="Ingresa una tarea y oprime Enter" onChange={this.updateName.bind(this)}/>
            </form>
         </div>
       </div>
     )
   }
+
 }
 
 
